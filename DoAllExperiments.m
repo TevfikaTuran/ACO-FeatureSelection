c3c7a13c2c8bcc2c1c1de693aca0ff397450bb9f
Tevfika TURAN <89084854+TevfikaTuran@users.noreply.github.com>
2022-05-25 22:10:10 +0300

    Delete DoAllExperiments.m

--- DoAllExperiments.m
+++
@@ -1,9 +0,0 @@
-function acc = DoAllExperiments(X, y, numExperiments, numFolds, mdl_type)
-
-acc = zeros(numExperiments,1);
-
-for ind= 1:numExperiments
-    acc_all = DoAllFolds(X, y, numFolds, mdl_type);
-    acc(ind) = mean(acc_all);
-end
-acc = mean(acc);
